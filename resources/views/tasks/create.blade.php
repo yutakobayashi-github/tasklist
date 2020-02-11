36fcda32bd7c7c7624f0cb30c6bf415dd7d8e361
tyuta.kobayashi <taro.kirameki@techacademy.jp>
2020-02-11 18:06:14 +0000

    tasklist

---
+++ resources/views/tasks/create.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+
+<!-- ここにページ毎のコンテンツを書く -->
+<h1>メッセージ新規作成ページ</h1>
+
+    <div class="row">
+        <div class="col-6">
+            
+            <!-- Form::model() は第一引数に対象となる Model のインスタンス -->
+            {!! Form::model($task, ['route' => 'tasks.store']) !!}
+        
+                <div class="form-group">
+                    {!! Form::label('content', 'メッセージ:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+        
+                {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
+        
+            {!! Form::close() !!}
+        </div>
+    </div>
+    
+@endsection
